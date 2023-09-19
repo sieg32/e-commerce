0e64cdc6fad1cf1e068fe409cff6fb13b6ba1ce4
yogesh <yogeshmalviya.raptor@gmail.com>
2023-09-19 19:43:59 +0530

    initial login and registration working

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const user = require('../db_schema/user_schema');
+const { response } = require('express');
 
 loginPage = (req, res)=>{
     let current = __dirname
@@ -10,23 +11,43 @@ loginPage = (req, res)=>{
 
 
 createUser = async(req,res)=>{
-    const nigga = req.body;
-    console.log("user created ");
-    try{
-        await user.create(nigga);
-
-    }catch(error){
-        console.log(error);
+    const temp_check = await user.find({username: req.body.username});
+    console.log(temp_check.length);
+    if(temp_check.length===0){
+       
+        const tempUserData = req.body;
+        console.log(tempUserData);
+        try{
+            await user.create(tempUserData);
+            
+        }catch(error){
+            console.log(error);
+        }
+        res.status(200).send({"login":"success"});
+    }else
+    {
+        res.send({"login":"user already exist" });
     }
+    
 
-     res.status(200).send({msg:"created"});
 
 }
 
 
 loginUser = async(req,res)=>{
-    console.log(req.body);
-    res.send(req.body);
+    const found_user = await user.find({username:req.body.username})
+    if(found_user.length === 0){
+        res.send({"login": "username does not exist"});
+    }else{
+        if(found_user[0].password === req.body.password){
+            res.send({"login": "success" })
+        }else{
+            res.send({"login":"password incorrect"})
+        }
+        
+    }
+
+    
 
 }
 
--- static/login_script.js
+++ static/login_script.js
@@ -3,7 +3,66 @@ let registered = true;
 
 const lform = document.querySelector('#login-form')
 
+const username_box = document.querySelector('#username');
+const doesnt_exist = document.querySelector('#doesnt-exist');
+const already_exist = document.querySelector('#already-exist');
+const pw_incorrect = document.querySelector('#pw_incorrect');
+const password_box = document.querySelector('#password');
+
+
+username_box.addEventListener('focus',()=>{
+    
+    doesnt_exist.classList.add("hidden");
+    already_exist.classList.add("hidden");
+})
+
+password_box.addEventListener('focus',()=>{
+    pw_incorrect.classList.add('hidden');
+})
+
+
+
+
+
+
+
 const login_text= document.querySelector('#textlogin');
+
+sendData=(url,form)=>{
+    axios.post(url, form,{
+        Headers:{
+            'content-type':'application/json',
+
+        }
+    }).then((response)=>{
+        console.log(response);
+        if(response.data.login === "success"){
+            console.log("logged in");
+            window.alert('logged in');
+
+        }else if(response.data.login === "username does not exist"){
+            console.log("does not exist")
+           
+            console.log(doesnt_exist);
+            doesnt_exist.classList.remove('hidden');
+        }else if(response.data.login === "password incorrect"){
+            console.log("password incorrect");
+            pw_incorrect.classList.remove('hidden');
+            
+        }else if(response.data.login === "user already exist"){
+
+            console.log("already exists")
+            already_exist.classList.remove("hidden");
+        }
+        
+
+       
+    }).catch((error)=>{
+        console.log("error", error)
+    })
+    
+}
+
 switchRegister = function(){
 
   
@@ -12,6 +71,7 @@ switchRegister = function(){
     email.placeholder = "email";
     email.type="email";
     email.name="email";
+    email.required = true;
     
     const input_form = document.querySelector('#inputs form')
     
@@ -47,37 +107,23 @@ const form_login = document.querySelector('#login_form');
 form_login.addEventListener('submit', (event)=>{
     event.preventDefault();
 
-    console.log(form_login);
+   
     const formdata = new FormData(document.querySelector('#login_form'));
     const form_obj = {};
 
     formdata.forEach((value,key)=>{
         form_obj[key]=value;
     })
-    console.log(form_obj);
+   
+    const Response_login ={};
 
     if(registered){
-        axios.post('/user/login', form_obj,{
-            Headers:{
-                'content-type':'application/json',
-
-            }
-        }).then((response)=>{
-            console.log(response)
-        }).catch((error)=>{
-            console.log("error", error)
-        })
+       sendData('/user/login',form_obj);
+       
     }else{
-        axios.post('/user/register', form_obj,{
-            Headers:{
-                'content-type':'application/json',
-
-            }
-        }).then((response)=>{
-            console.log(response)
-        }).catch((error)=>{
-            console.log("error", error)
-        })
+       sendData('/user/register', form_obj);
+       
+      
     }
     
 
