d44867c9fb74de3a2ce34dbff56ef149cc508583
yogesh <yogeshmalviya.raptor@gmail.com>
2023-09-20 23:50:08 +0530

    small changes

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,98 +1,95 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const user = require('../db_schema/user_schema');
-const { response } = require('express');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const user = require("../db_schema/user_schema");
+const { response } = require("express");
 
-
-
-createToken= (username, email)=>{
-
-    const token = jwt.sign({
-        "username": username,
-        "email":email
-        }, process.env.JWT_SECRET_KEY,{
-            expiresIn:"6h",
-        })
-        console.log(token);
-    
-        return token;
-
-}
-
-loginPage = (req, res)=>{
-    let current = __dirname
-     let Root = current.slice(0,-12);
-    res.sendFile(Root +'/login.html');
-       
-}
-profilePage= (req,res)=>{
-    let current = __dirname
-    let Root = current.slice(0,-12);
-   res.sendFile(Root +'/profile.html');
-
-}
-
-
-createUser = async(req,res)=>{
-    const temp_check = await user.find({username: req.body.username});
-    console.log(temp_check.length);
-    if(temp_check.length===0){
-       
-        const tempUserData = req.body;
-        console.log(tempUserData);
-        try{
-            await user.create(tempUserData);
-            
-        }catch(error){
-            console.log(error);
-        }
-       const token = createToken(req.body.username, req.body.email);
-
-        res.status(200).send({"login":"success","username":req.body.username, "token": token});
-    }else
+createToken = (username, email) => {
+  const token = jwt.sign(
     {
-        res.send({"login":"user already exist" });
+      username: username,
+      email: email,
+    },
+    process.env.JWT_SECRET_KEY,
+    {
+      expiresIn: "6h",
     }
-    
+  );
+  console.log(token);
+
+  return token;
+};
 
 
-}
 
 
-loginUser = async(req,res)=>{
-    const found_user = await user.find({username:req.body.username})
-    if(found_user.length === 0){
-        res.send({"login": "username does not exist"});
-    }else{
-        const token = createToken(req.body.username, found_user[0].email)
+loginPage = (req, res) => {
+  let current = __dirname;
+  let Root = current.slice(0, -12);
+  res.sendFile(Root + "/login.html");
+};
+profilePage = (req, res) => {
+  let current = __dirname;
+  let Root = current.slice(0, -12);
+  res.sendFile(Root + "/profile.html");
+};
 
-        if(found_user[0].password === req.body.password){
-            res.send({"login": "success", "username":req.body.username, "token": token })
-        }else{
-            res.send({"login":"password incorrect"})
-        }
-        
+
+
+
+
+createUser = async (req, res) => {
+  const temp_check = await user.find({ username: req.body.username });
+  console.log(temp_check.length);
+  if (temp_check.length === 0) {
+    const tempUserData = req.body;
+    console.log(tempUserData);
+    try {
+      await user.create(tempUserData);
+    } catch (error) {
+      console.log(error);
     }
+    const token = createToken(req.body.username, req.body.email);
 
-    
+    res
+      .status(200)
+      .send({ login: "success", username: req.body.username, token: token });
+  } else {
+    res.send({ login: "user already exist" });
+  }
+};
 
-}
 
-getUserInfo =async (req, res)=>{
-    console.log(res.locals);
-    const userData = await user.find({"username": res.locals.username});
-   
-     const  {username, email, _id} = userData[0];
-    console.log(userData[0]);
-    
-    console.log(_id);
 
-    res.send({"authorization":"success",
-            "userdata":{ username,
-            "email":email,
-            "id":_id}
 
-    });
-}
 
-module.exports = {loginPage, createUser, loginUser, profilePage};
+loginUser = async (req, res) => {
+  const found_user = await user.find({ username: req.body.username });
+  if (found_user.length === 0) {
+    res.send({ login: "username does not exist" });
+  } else {
+    const token = createToken(req.body.username, found_user[0].email);
+
+    if (found_user[0].password === req.body.password) {
+      res.send({ login: "success", username: req.body.username, token: token });
+    } else {
+      res.send({ login: "password incorrect" });
+    }
+  }
+};
+
+
+
+
+
+getUserInfo = async (req, res) => {
+  const userData = await user.find({ username: res.locals.username });
+
+  const { username, email, _id } = userData[0];
+
+  res.send({
+    authorization: "success",
+    userdata: { username, email: email, id: _id },
+  });
+};
+
+module.exports = { loginPage, createUser, loginUser, profilePage };
--- static/profile_page.js
+++ static/profile_page.js
@@ -1,11 +1,56 @@
+let userData = {};
+const nameUser = document.querySelector('#name');
+const emailUser = document.querySelector('#email');
+const securityBtn = document.querySelector('#securityBtn');
+const profileBtn = document.querySelector('#profileBtn');
+const profileBox = document.querySelector('#profile');
+const privacyBox=document.querySelector('#privacy');
+const homeBtn = document.querySelector('#homeBtn');
 
-axios.get('/user/getUserInfo',{
-    headers:{
-        author : localStorage.getItem("token")
-    }
-}).then((response)=>{
-    console.log(response);
-    if(response.data.authorization === "failed" || response.data.authorization === "token expire"){
-        console.log("nigga your expired")
-    }
+
+
+
+const populateUsername = async()=>{
+   await axios.get('/user/getUserInfo',{
+        headers:{
+            author : localStorage.getItem("token")
+        }
+    }).then((response)=>{
+        console.log(response);
+        if(response.data.authorization === "failed" || response.data.authorization === "token expire"){
+            console.log("nigga your expired")
+            
+            window.location.href = ('/user/login');
+        }else{
+            userData = response.data.userdata;
+        }
+    })
+    
+    nameUser.textContent = userData.username;
+    email.textContent = userData.email;
+   
+}
+
+populateUsername()
+
+securityBtn.addEventListener('click',()=>{
+    profileBtn.classList.remove('focus');
+    securityBtn.classList.add('focus');
+
+    profileBox.style.display = "none";
+    privacyBox.style.display = "flex";
+
+
+})
+
+profileBtn.addEventListener('click',()=>{
+    securityBtn.classList.remove('focus');
+    profileBtn.classList.add('focus');
+
+    profileBox.style.display="flex";
+    privacyBox.style.display="none";
+})
+
+homeBtn.addEventListener('click',()=>{
+    window.location.href= "/";
 })
