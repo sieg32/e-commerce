732e324bdc8a6e91f440972c4c7d8f81182bcc6d
yogesh <yogeshmalviya.raptor@gmail.com>
2023-09-22 17:58:47 +0530

    extracted header in separate file

--- static/product_page.js
+++ static/product_page.js
@@ -1,14 +1,11 @@
-const url = window.location.href.split('/');
-
-const param = url[url.length -1];
-
+let ProductInfo;
 const populateData = (data)=>{
     const name = document.querySelector('#name h2');
     const price = document.querySelector('#price h4');
     const rating = document.querySelector('#price h5');
     const description = document.querySelector('#description p');
     const Pagetitle = document.querySelector('head title');
-
+    
     Pagetitle.textContent = data.product_name;
     name.textContent = data.product_name;
     price.textContent = "₹"+data.price;
@@ -22,12 +19,17 @@ const populateData = (data)=>{
 
 
 
-const getProductinfo =(param)=>{
+const getProductinfo = async()=>{
      
-    axios.get('/products/api/'+ param).then(
+    const url = window.location.href.split('/');
+    
+    
+    const param = url[url.length -1];
+   await axios.get('/products/api/'+ param).then(
         (response)=>{
             console.log(response);
             populateData(response.data);
+            ProductInfo = response.data;
         }
     )
  }
@@ -35,7 +37,7 @@ const getProductinfo =(param)=>{
  const buyProduct = (productName)=>{
     console.log(productName);
  }
- getProductinfo(param)
+ getProductinfo();
 
 
 
@@ -46,6 +48,6 @@ const getProductinfo =(param)=>{
 
  buyBtn.addEventListener('click',(elem)=>{
 
-    buyProduct(param);
+    buyProduct(ProductInfo.product_name);
     
  })
